e3a94f78fa3f564286c507764772b59e152dcd3f
ProtoAce <mishagoldenberg@gmail.com>
2022-04-07 08:34:06 -0700

    Added prototype for message scraper
    Has not been tested yet, train ride is over...

--- js/bot.js
+++ js/bot.js
@@ -7,6 +7,48 @@ client.once('ready', () => {
     console.log("socialCredit is online!")
 });
 
+//function retunrs all messages from channelID sent by specified userID
+//if userID is '-1' function returns all messages from all users.
+function getAllMessages(channelID, userID){
+    const channel = client.channel.cache.get(channelID);
+    let messages = [];
+    let message = await channel.messages.fetch({limit:1})
+        .then(messagePage => (messagePage === 1 ? messagePage.at(0) : null));
+
+    while(message){
+        await channel.messages.fetch({limit:100})
+            .then(messagePage => {
+                messagePage.forEach(msg => storeMessage(msg, messages));
+
+                message = 0 < messagePage.size ? messagePage.at(messagePage.size -1) : null;
+            })
+    }
+    return messages;
+};
+
+//storeMessage stores msg into the messages array according to user ID
+function storeMessage(msg, messages){
+    let accountID = msg.author.id;
+    let message = msg.content;
+
+    console.log(user, message);
+
+    let index = messages.indexOf(user);
+    //first message from this user
+    if(index === -1){
+        messages.push({user: accountID, messages: [message]})
+    }else{
+        messages[index] = {user: accountID, messages: [messages.split(), message]};
+    }
+}
+
+//storeMessagesToJSON converts messages to JSON type
+function storeMessagesToJSON(messages){
+    let messageJSON = JSON.stringify(messages);
+    return messageJSON;
+}
+
+
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
