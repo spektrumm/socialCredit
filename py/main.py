0327bd8051c104096c1aa81f876cb2d77b423ef3
spektrumm <51189697+spektrumm@users.noreply.github.com>
2022-04-10 19:48:05 -0700

    json parsing and verifying flair works

--- py/main.py
+++ py/main.py
@@ -1,4 +1,25 @@
-import random
-import time
-from bs4 import BeautifulSoup
+import json
+from flair.data import Sentence
+from flair.models import TextClassifier
 
+sia = TextClassifier.load("en-sentiment")
+
+
+def flairPrediction(x):
+    sent = Sentence(x)
+    sia.predict(sent)
+    score = sent.labels[0]
+    if "POSITIVE" in str(score):
+        return "pos"
+    elif "NEGATIVE" in str(score):
+        return "neg"
+    else:
+        return "neu"
+
+
+with open("msg.json", 'r') as jsonFile:
+    data = json.load(jsonFile)
+
+    tempC = data['content']
+    msgContent = tempC[1:-1]
+    print(msgContent)
