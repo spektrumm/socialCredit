27346cf37ac621a9e0d9137cd3898d0d4d709793
spektrumm <51189697+spektrumm@users.noreply.github.com>
2022-04-11 17:55:19 -0700

    looping through bulk msg files  + sentiment an on indexed messages

--- py/bulk.py
+++ py/bulk.py
@@ -1,4 +1,53 @@
 import nlpFunc as nlp
 import json
-
+import os
+import shutil
 # this will be a standalone script for processing previously sent messages in bulk.
+
+
+# start of script inits
+sentAn = nlp.classifierInit('en-sentiment')
+dir = 'messageJsons'
+
+
+def stepThroughMsgs(fileName):
+    sourcePath = f'messageJsons/{fileName}'
+    if os.path.exists(sourcePath):
+        destPath = fileName
+        newLocation = shutil.move(sourcePath, destPath)
+        print('The incoming %s file has been moved to the root directory %s' %
+              (sourcePath, newLocation))
+    with open(fileName, 'r') as jsonFile:
+        data = json.load(jsonFile)
+        return data
+
+
+# scrape directory of files
+allChannels = os.listdir(path=dir)
+channelIndex = 0
+scoresDict = {}
+for channelFiles in allChannels:
+    msgCount = 0
+    channelMsgs = allChannels[channelIndex]
+    channelIndex += 1
+    jsonData = stepThroughMsgs(channelMsgs)
+    if jsonData != None:
+        for messages in jsonData:
+            currentMsg = jsonData[msgCount]
+            msgCount += 1
+            msgCon = currentMsg['content']
+            userID = currentMsg['authorID']
+            if msgCon != '':
+
+                # do sentiment analysis on msgCon
+                predictScore = nlp.flairPrediction(msgCon, sentAn)
+                # take sentAn score and append to dict keyed to userID
+                if userID in scoresDict:
+                    oldScore = scoresDict[userID]
+                    newScore = oldScore + predictScore
+                    scoresDict.update({userID: newScore})
+                else:  # if user id doesnt already exist
+                    scoresDict.update({userID: predictScore})
+
+with open('legacyScores.json', 'w') as jsonOut:
+    jsonObj = json.dumps(scoresDict, indent=4)
