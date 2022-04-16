6dbfca00b464a611d188b1e976a05cd7b54ff997
spektrumm <51189697+spektrumm@users.noreply.github.com>
2022-04-16 13:18:32 -0700

    fixed scoreFromDB and getSocialCredit errors

--- js/commands/getSocialCredit.js
+++ js/commands/getSocialCredit.js
@@ -2,9 +2,11 @@ const scoreFromDb = require('.../functions/scoreFromDb.js');
 
 //sends a message with user's score to channel.
 module.exports.run = async (client, message, cmd, args, db) => {
+    let user = getUserFromMention.run(client, args[0]);
+    let userID = user.id;
 
-    userScore = scoreFromDb(client, args, db);
-    message.channel.send(`<@${userId}>'s Social Credit is ${userScore}.`);
+    userScore = scoreFromDb(client, args, db, userID);
+    message.channel.send(`<@${userID}>'s Social Credit is ${userScore}.`);
     
 };
 
--- js/functions/scoreFromDb.js
+++ js/functions/scoreFromDb.js
@@ -1,14 +1,13 @@
-module.exports = async function (client, args, db){
-    let user = getUserFromMention.run(client, args[0]);
-    let userID = user.id;
+module.exports = async function (client, args, db, userID){
 
     let sql = `SELECT score FROM users WHERE userId = ${userID}`;
     await db.query(sql, function(err, result, fields) {
         if(err){
             throw err
         }
-        let score = result[0].score;
-        return score; 
+        let userScore = result[0].score;
+        return userScore; 
 
     });
+    return userScore;
 }
