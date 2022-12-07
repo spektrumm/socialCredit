e32fc453c861818a269cf3f84227bb2e87b43cfd
Misha <mishagoldenberg@gmail.com>
2022-12-06 20:07:10 -0800

    upgraded chartjs + improved chart

--- js/functions/getUserDataByName.js
+++ js/functions/getUserDataByName.js
@@ -4,7 +4,7 @@ module.exports = async function (userName, db) {
   let sql = `SELECT userId FROM users WHERE name = ?`;
   const result = await dbQuery(db, sql, [userName]);
   const userId = result[0].userId;
-  let sql2 = `SELECT score, timestamp FROM messages WHERE userId = ? ORDER BY score ASC`;
+  let sql2 = `SELECT score, timestamp FROM messages WHERE userId = ? ORDER BY timestamp ASC`;
   console.log(userId);
   return await dbQuery(db, sql2, [userId]);
 };
--- react/social-credit-app/src/components/userScreen/chart.js
+++ react/social-credit-app/src/components/userScreen/chart.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { GetUserDataByName } from "../../requests";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS, Ticks, TimeScale } from "chart.js/auto";
 import useWindowDimensions from "../useWindowsDimentions";
+import "chartjs-adapter-date-fns";
 
 const Chart = (userName) => {
   const { height, width } = useWindowDimensions();
@@ -22,55 +23,73 @@ const Chart = (userName) => {
   }, []);
 
   if (user !== undefined) {
-    let xValues = [];
-    let yValues = [];
-    // console.log(user);
+    let messages = [];
+
     user.forEach((message) => {
-      xValues.push(message.timestamp);
-      yValues.push(message.score);
-      //   messages.push({ x: message.timestamp, y: message.score });
+      let d = new Date(0);
+      d.setUTCMilliseconds(message.timestamp);
+      messages.push({ x: d, y: message.score });
     });
-    // console.log(messages);
-    // const data = {
-    //   //labels: labels,
-    //   datasets: [
-    //     {
-    //       label: "My First Dataset",
-    //       data: [
-    //         { x: 80, y: 90 },
-    //         { x: 81, y: 29 },
-    //         { x: 56, y: 36 },
-    //         { x: 55, y: 25 },
-    //         { x: 40, y: 18 },
-    //       ],
-    //       fill: false,
-    //       borderColor: "rgb(75, 192, 192)",
-    //       tension: 0.1,
-    //     },
-    //   ],
-    // };
 
     const data = {
-      labels: xValues,
       datasets: [
         {
           label: "Social Credit Vs Time",
-          data: yValues,
+          data: messages,
           fill: true,
-          backgroundColor: "rgba(75,192,192,0.2)",
-          borderColor: "rgba(75,192,192,1)",
+          // backgroundColor: "rgba(75,192,192,0.2)",
+          // backgroundColor: (context) => {
+          //   const ctx = context.chart.ctx;
+          //   const gradient = ctx.createLinearGradient(0, 0, 0, height);
+          //   gradient.addColorStop(0, "rgba(75,192,192,1)");
+          //   gradient.addColorStop(1, "rgba(27,27,39,0)");
+          //   return gradient;
+          // },
+          segment: {
+            borderColor: (ctx) => {
+              return ctx.p0.parsed.y >= 0
+                ? "rgba(75,192,192,1)"
+                : "rgba(248,51,60,1)";
+            },
+            backgroundColor: (ctx) => {
+              return ctx.p0.parsed.y >= 0
+                ? "rgba(75,192,192,0.2)"
+                : "rgba(248,51,60,0.2)";
+            },
+          },
+          pointRadius: 0,
+          borderWidth: 1,
+          tension: 1,
         },
       ],
+    };
+    const options = {
+      scales: {
+        x: {
+          type: "time",
+          time: {
+            unit: "day",
+            unitStepSize: 1,
+            displayFormats: {
+              day: "MM/dd/yyyy",
+            },
+          },
+          offset: true,
+          ticks: {
+            maxTicksLimit: 20,
+          },
+        },
+      },
+      layout: { autoPadding: 100 },
+      responsive: true,
+      maintainAspectRatio: true,
+      parsed: true,
     };
 
     return (
       <div style={{ width: width * 0.8, height: height * 0.9 }}>
         <Line
-          options={{
-            responsive: true,
-            maintainAspectRatio: true,
-            //layout: { autoPadding: 100 },
-          }}
+          options={options}
           //style={{ padding: 10 }}
           data={data}
         />
