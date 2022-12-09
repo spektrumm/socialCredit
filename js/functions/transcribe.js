ac1fc9d337022979edc70129d70be64d9f542318
Misha Goldenberg <mishagoldenberg@gmail.com>
2022-12-08 18:27:21 -0800

    transcribe function

--- js/functions/transcribe.js
+++ js/functions/transcribe.js
@@ -1,23 +1,103 @@
 const fs = require("fs");
-const DeepSpeech = require("deepspeech");
+// const { setTimer, getTimer } = require("chronos");
+const STT = require("stt");
 
-// // Path to the DeepSpeech model and alphabet files
-// const MODEL_FILE = "./models/output_graph.pbmm";
-// const ALPHABET_FILE = "./models/alphabet.txt";
+function loadModel(modelPath, scorerPath) {
+  let model;
 
-// // Load the DeepSpeech model and alphabet
-// const model = new DeepSpeech.Model(MODEL_FILE);
-// const alphabet = fs.readFileSync(ALPHABET_FILE, "utf-8");
+  try {
+    model = new STT.Model(modelPath);
+  } catch (error) {
+    throw `model.stt: ${error}`;
+  }
 
-// // Load the PCM audio file and convert it to the correct format
-// const audioBuffer = fs.readFileSync("./230449859809902592-audio");
-// const audioLength = audioBuffer.length / 2;
-// const audioData = new Int16Array(audioLength);
+  try {
+    model.enableExternalScorer(scorerPath);
+  } catch (error) {
+    throw `model.enableExternalScorer: ${error}`;
+  }
 
-// for (let i = 0; i < audioLength; i++) {
-//   audioData[i] = audioBuffer.readInt16LE(i * 2);
+  return model;
+}
+
+function transcriptBuffer(audioBuffer, model) {
+  // WARNING:
+  // no audioBuffer validation is done.
+  // The audio fle must be a WAV audio in raw format.
+
+  try {
+    return model.stt(audioBuffer);
+  } catch (error) {
+    throw `model.stt error: ${error}`;
+  }
+}
+
+function freeModel(model) {
+  try {
+    STT.FreeModel(model);
+  } catch (error) {
+    throw `STT.FreeModel error: ${error}`;
+  }
+}
+
+module.exports = (file) => {
+  const modelPath = "C:\\repos\\socialCredit\\js\\ttsModels\\model.tflite";
+  const scorerPath =
+    "C:\\repos\\socialCredit\\js\\ttsModels\\huge-vocabulary.scorer";
+  let result;
+
+  //   const loadModelTime = setTimer();
+
+  //
+  // load Coqui STT model
+  //
+
+  const model = loadModel(modelPath, scorerPath);
+  let audioBuffer = fs.readFileSync(file);
+
+  //   if (DEBUG_WORKER) console.log(`worker: load model elapsed:`);
+
+  //
+  // transcript the audio buffer
+  //
+  try {
+    result = transcriptBuffer(audioBuffer, model);
+
+    // if (DEBUG_WORKER) console.log(`worker: transcriptBuffer elapsed:`);
+  } catch (error) {
+    throw `transcriptBuffer: ${error}`;
+  }
+
+  //
+  // free the model
+  //
+
+  freeModel(model);
+
+  return result;
+};
+
+// function main() {
+//   const modelPath = "C:\\repos\\socialCredit\\js\\ttsModels\\model.tflite";
+//   const scorerPath =
+//     "C:\\repos\\socialCredit\\js\\ttsModels\\huge-vocabulary.scorer";
+//   const sourceFile =
+//     "C:\\repos\\socialCredit\\js\\230449859809902592-pelase.wav";
+
+//   //   const modelPath = "./../ttsModels/coqui-stt-0.9.3-models.pbmm";
+//   //   const scorerPath = "./../ttsModels/coqui-stt-0.9.3-models.scorer";
+//   //   const sourceFile = "./230449859809902592-audio.wav";
+
+//   //
+//   // load an audio file into a Buffer
+//   //
+//   const audioBuffer = fs.readFileSync(sourceFile, { flag: "rs+" });
+
+//   const result = transcriptNoThread(modelPath, scorerPath, audioBuffer);
+
+//   console.log(`transcript: ${result}`);
 // }
 
-// // Run the transcription
-// const transcription = model.stt(audioData, 16000, alphabet);
-// console.log(transcription);
+// if (require.main === module) main();
+
+// module.exports = { transcriptNoThread };
